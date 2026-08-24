263f65d043461cbd271ebbcd920d93ef5ddddb18
Modar Dwery <modar@example.com>
2026-08-24 09:19:52 +0300

    Add customer statement report

---
+++ customer_statement_report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Customer Statement Report',
+    'version': '19.0.1.0.0',
+    'category': 'Accounting',
+    'summary': 'Customer statement report with product details',
+    'author': 'Magic Quantum',
+    'depends': ['account', 'sale'],
+    'data': [
+        'security/ir.model.access.csv',
+        'wizard/customer_statement_wizard_views.xml',
+        'view/account_move_line_view.xml',
+        'report/customer_statement_report.xml',
+        'report/customer_statement_templates.xml',
+    ],
+    # 'assets': {
+    #     'web.report_assets_common': [
+    #         'customer_statement_report/static/src/css/statement_report.css',
+    #     ],
+    # },
+    'installable': True,
+    'license': 'LGPL-3',
+}
